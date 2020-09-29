a500bc960a64a02cb917f7d58cedf5fff6655c33
nikskushwah <nikskushwah@gmail.com>
2020-09-29 14:14:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("hello world")
+from flask import Flask
+
+module= Flask(__name__)
+
+ 
+
+@module.route("/")
+def hello():
+ return "Hello from FastCGI via IIS!"
+
+ 
+
+if __name__ == '__main__':
+    module.run()
